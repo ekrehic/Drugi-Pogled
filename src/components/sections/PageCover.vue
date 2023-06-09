639ebc9c3a75348abb187bf8f91ef2dad00fcfb0
Emina Krehic <emina.krehic@foreo.com>
2023-06-09 15:12:37 +0200

    cover

--- src/components/sections/PageCover.vue
+++ src/components/sections/PageCover.vue
@@ -1,15 +1,32 @@
 <template>
     <div class="cover">
-      <div class="cover__bg"></div>
       <div class="cover__content section">
-        <img class="logo" src="@/assets/images/RiverDrina_text.png" alt="RIVER DRINA"/>
-        <img class="wave" src="@/assets/images/wave-yellow.png" alt="Yellow river drina wave"/>
-        <h1 v-html="$t(`cover.text`)"></h1>
+        <img class="logo" src="@/assets/images/drugi-pogled-logo.png" alt="DRUGI POGLED"/>
+        <h3 >
+          Drugi pogled nudi usluge
+          <span class="has-text-weight-semibold">
+            individualnog i partnerskog savjetovanja
+          </span>
+           koristeći metode i tehnike
+          <span class="has-text-weight-semibold">
+            Gestalt terapije
+          </span>
+          .
+          <span class="is-block"></span>
+          Savjetovanje je namijenjeno
+          <span class="has-text-weight-semibold">
+            mladima i odraslima
+          </span>
+          , a s ciljem unapređenja mentalnog zdravlja te
+          <span class="has-text-weight-semibold">
+            poboljšanja kvalitete života
+          </span>
+          naših klijenata.
+        </h3>
         <button class="button is-large is-green" @click="modals.contact = true">
-          {{ $t(`cover.cta`) }}
+          Zatražite termin
         </button>
       </div>
-      <div class="waves-animation"></div>
       <b-modal v-model="modals.contact">
         <div class="modal__body">
           <div class="is-flex is-justify-content-center is-align-items-center p-b-20">
@@ -42,24 +59,12 @@ export default PageCover;
 <style lang="scss">
 .cover {
   position: relative;
-  height: calc(100vh - 80px);
-
-  .cover__bg {
-    position: absolute;
-    width: 100%;
-    height: calc(100vh - 80px);
-    background-image: url("images/River_Drina-cover.png");
-    background-size: cover;
-    background-position: center;
-
-    @media screen and (max-width: 1024px) {
-      height: calc(100vh - 56px);
-    }
-
-  }
+  height: calc(100vh - 72px);
+  background-image: url("images/DrugiPogled-cover.png");
+  background-position: bottom;
 
   @media screen and (max-width: 1024px) {
-    height: calc(100vh - 56px);
+    min-height: calc(100vh - 56px);
   }
 
   .cover__content {
@@ -70,25 +75,18 @@ export default PageCover;
     justify-content: center;
     z-index: 5;
     position: relative;
+    padding: 40px 40px 80px;
 
     .logo {
-      max-width: 800px;
+      max-width: 380px;
       margin-bottom: 20px;
 
-      @media (min-resolution: 108dpi) {
-        max-width: 600px;
-      }
-
-      @media screen and (max-width: 1316px) {
-        max-width: 600px;
-      }
-
       @media screen and (max-width: 1024px) {
-        max-width: 400px;
+        max-width: 300px;
       }
 
       @media screen and (max-width: 768px) {
-        max-width: 300px;
+        max-width: 200px;
       }
 
       @media screen and (max-width: 420px) {
@@ -106,90 +104,34 @@ export default PageCover;
 
     }
 
-    h1 {
-      color: #ffffff;
+    h3 {
+      //color: #ffffff;
       margin-bottom: 30px;
-      max-width: 1000px;
+      //max-width: 1000px;
       text-align: center;
-      font-size: 38px;
-      text-shadow: 1px 1px 1px #4B778D;
+      //font-size: 20px;
+     // text-shadow: 1px 1px 1px rgba(34, 47, 79, 0.2);
 
-      @media (min-resolution: 108dpi) {
-        font-size: 30px;
-      }
+      //@media (min-resolution: 108dpi) {
+      //  font-size: 30px;
+      //}
 
-      @media screen and (max-width: 1024px) {
-        font-size: 30px;
-      }
-
-      @media screen and (max-width: 768px) {
-        font-size: 26px;
-      }
-
-      @media screen and (max-width: 420px) {
-        font-size: 24px;
-      }
+      //@media screen and (max-width: 1024px) {
+      //  font-size: 30px;
+      //}
+      //
+      //@media screen and (max-width: 768px) {
+      //  font-size: 26px;
+      //}
+      //
+      //@media screen and (max-width: 420px) {
+      //  font-size: 24px;
+      //}
     }
 
     .button {
       box-shadow: 0 0 4px #4B778D;
     }
-  }
-}
-
-.waves-animation {
-  position: absolute;
-  width: 100%;
-  height: 143px;/*PNG*/
-  bottom: -5px;
-  left:0;
-  background-image: url('images/wave-animation.png');
-  //background-size: contain;
-  animation: anim 10s linear infinite;
-}
-.waves-animation:before{
-  content: '';
-  position: absolute;
-  width: 100%;
-  height: 143px;
-  top:0;
-  left:0;
-  background-image: url('images/wave-animation.png');
-  opacity: 0.4;
-  animation: anim-rev 10s linear infinite;
-
-}
-.waves-animation:after{
-  content: '';
-  position: absolute;
-  width: 100%;
-  height: 143px;
-  top:0;
-  left:0;
-  background-image: url('images/wave-animation.png');
-  opacity: 0.6;
-  animation-delay: -5s;
-  animation: anim 20s linear infinite;
-
-}
-@keyframes anim {
-  0%
-  {
-    background-position: 0;
-  }
-  100%
-  {
-    background-position: 1360px;
-  }
-}
-@keyframes anim-rev {
-  0%
-  {
-    background-position: 1360px;
-  }
-  100%
-  {
-    background-position: 0;
   }
 }
 </style>
